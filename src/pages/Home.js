747078eb85391989a5b55d48b05b42814a79b50e
brockmolmen <brockmolmen@gmail.com>
2021-01-11 17:28:24 -0800

    add CSS for resposivness on logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="bump">
-      <img src="logo-name.png" alt="logo" className="main-logo" />
+      <img src="logo-name.svg" alt="logo" className="main-logo" />
     </div>
   )
 }

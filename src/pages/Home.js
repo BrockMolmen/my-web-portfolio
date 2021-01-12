fc4a67c64240b6cc15fb4029e61b61773d72f2a8
brockmolmen <brockmolmen@gmail.com>
2021-01-11 16:08:32 -0800

    rebuild

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="bump">
+      <img src="logo-name.svg" alt="logo" className="main-logo" />
+    </div>
+  )
+}
+
+export default Home

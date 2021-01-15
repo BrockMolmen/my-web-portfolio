4dcb8e13879e23b35b1d8502dcff3ab8bed4af97
brockmolmen <brockmolmen@gmail.com>
2021-01-14 18:04:26 -0800

    touched up design

--- src/pages/About.js
+++ src/pages/About.js
@@ -28,25 +28,25 @@ const About = () => {
               </ul>
             </div>
             <p>My name is Brock. I am a full-stack software engineer, focused on innovative front-end development. I enjoy writing code that looks as good as it functions. Transitioning from a career in the ever-evolving airline industry, I’m able to adapt and learn quickly in new situations and rely on great communication to accomplish goals in a team.</p>
-            <p>I recently moved from Minneapolis, MN to Seattle, WA with my husband and dog, Gus. When I’m not behind my computer, I enjoy collecting and playing retro video games, trekking around the globe, skateboarding with my dog, and getting deep in conversations about music. I frequently use outdated slang like “rad” and “boss.” Let's grab a cold press and chat.</p>
+            <p>I recently moved from Minneapolis, MN to Seattle, WA with my husband and dog, Gus. When I’m not behind my computer, I enjoy collecting and playing retro video games, trekking around the globe, skateboarding with my dog, and getting deep in conversations about music. I frequently use outdated slang like “rad” and “boss.” Let's grab a cold press coffee and chat.</p>
           </div>
         </div>
-        <div className="project-container stack-it">
+        <div className="project-container stack-it tech-card">
           <div>
-            <h1 className="project-title">Technical Skills</h1>
+            <h1 className="proj-title">Technical Skills</h1>
           </div>
           <div>
             <ul className="tech-skills-list">
-              <li> <FontAwesomeIcon icon={faHtml5} className="icon-1" size="2x" /> Html </li>
-              <li> <FontAwesomeIcon icon={faCss3Alt} className="icon-2" size="2x" /> CSS </li>
+              <li> <FontAwesomeIcon icon={faHtml5} className="icon-3" size="2x" /> Html </li>
+              <li> <FontAwesomeIcon icon={faCss3Alt} className="icon-3" size="2x" /> CSS </li>
               <li> <FontAwesomeIcon icon={faSass} className="icon-3" size="2x" /> Sass </li>
-              <li> <FontAwesomeIcon icon={faReact} className="icon-1" size="2x" /> React </li>
-              <li> <FontAwesomeIcon icon={faCogs} className="icon-2" size="2x" /> Express </li>
+              <li> <FontAwesomeIcon icon={faReact} className="icon-3" size="2x" /> React </li>
+              <li> <FontAwesomeIcon icon={faCogs} className="icon-3" size="2x" /> Express </li>
               <li> <FontAwesomeIcon icon={faLevelUpAlt} className="icon-3" size="2x" /> Sequelize </li>
-              <li> <FontAwesomeIcon icon={faNode} className="icon-1" size="2x" /> Node.js </li>
-              <li> <FontAwesomeIcon icon={faDatabase} className="icon-2" size="2x" /> PostgreSQL </li>
+              <li> <FontAwesomeIcon icon={faNode} className="icon-3" size="2x" /> Node.js </li>
+              <li> <FontAwesomeIcon icon={faDatabase} className="icon-3" size="2x" /> PostgreSQL </li>
               <li> <FontAwesomeIcon icon={faJsSquare} className="icon-3" size="2x" /> Javascript </li>
-              <li> <FontAwesomeIcon icon={faPython} className="icon-1" size="2x" /> Python </li>
+              <li> <FontAwesomeIcon icon={faPython} className="icon-3" size="2x" /> Python </li>
             </ul>
           </div>
         </div>
